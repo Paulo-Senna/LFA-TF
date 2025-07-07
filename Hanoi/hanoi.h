eb989aa96cc50bc93fe91059f9dd3e1c801fbd80
Paulo Senna <codesenna@gmail.com>
2025-07-06 22:16:17 -0300

    Remoção pasta AP, inclusão de pilha.c e edição geral da pasta Hanoi

--- Hanoi/hanoi.h
+++ Hanoi/hanoi.h
@@ -13,14 +13,18 @@ typedef struct {
     Pilha pilhas[NUM_PILHAS];
 } MultiPilha;
 
-void inicializarMultiPilha(MultiPilha *mp);
+void imprime_pilha(Pilha *p);
+void emp(Pilha *p, char elemento);
+char desemp(Pilha *p);
+void inicializa_pilha(Pilha *p);
+void inicializarPilha(MultiPilha *p);
 int estaVazia(MultiPilha *mp, int indice);
 int estaCheia(MultiPilha *mp, int indice);
 void empilhar(MultiPilha *mp, int indice, int valor);
 int desempilhar(MultiPilha *mp, int indice);
 int topo(MultiPilha *mp, int indice);
-void moverDiscos(int n, MultiPilha *mp, int origem, int auxiliar, int destino, char *str);
+void moverDiscos(int n, Pilha *p, MultiPilha *mp, int origem, int auxiliar, int destino, char *str);
 void imprimirEstado(MultiPilha *mp, int numDiscos);
-void reco( char * String, int orig, int dest);
+void reco(char *String, int orig, int dest);
 
 #endif
--- Hanoi/main.c
+++ Hanoi/main.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "hanoi.h"
 
 int main() {
+    Pilha p;
     MultiPilha mp;
     int numDiscos;
     char palavra[1000] = "";
+
     printf("Digite a quantidade de discos: ");
     scanf("%d", &numDiscos);
 
-    inicializarMultiPilha(&mp);
+    inicializa_pilha(&p);
+    inicializarPilha(&mp);
+
+    for (int i = 1; i <= numDiscos; i++) {
+        emp(&p, 'N');
+        printf("est. E");
+        desemp(&p);
+        printf("est. V");
+        emp(&p, 'T');
+    }
 
     for (int i = numDiscos; i >= 1; i--) {
         empilhar(&mp, 0, i);
@@ -17,10 +29,15 @@ int main() {
     printf("\nEstado Inicial: \n");
     imprimirEstado(&mp, numDiscos);
 
-    moverDiscos(numDiscos, &mp, 0, 1, 2, palavra);
+    moverDiscos(numDiscos, &p, &mp, 0, 1, 2, palavra);
 
-    printf("\nMovimentos: %s", palavra);
+    imprime_pilha(&p);
 
+    if (p.topo == -1) {
+        printf("\nPilha auxiliar est\u00e1 vazia!");
+    }
+
+    printf("\nMovimentos: %s\n", palavra);
 
     return 0;
 }
